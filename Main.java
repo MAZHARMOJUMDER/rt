9cb48b8c82ce96af8a87c3a134fb19eb34b1dfe1
MAZHARMOJUMDER <52679397+MAZHARMOJUMDER@users.noreply.github.com>
2019-07-29 13:08:38 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    String iphoneA = "3samsung";
+	    String iphoneB = "5samsung";
+	    String sum     = iphoneA + iphoneB;
+	 
+	 System.out.println( sum);
+	}
+}
