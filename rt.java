4ce263e7eba00dd25c292c884b088409a7ec1752
MAZHARMOJUMDER <52679397+MAZHARMOJUMDER@users.noreply.github.com>
2019-07-29 00:15:50 -0400

    Add files via upload
    first commit

---
+++ rt.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    String iphoneA = "3az";
+	    String iphoneB = "5az";
+	    String sum     = iphoneA + iphoneB;
+	 
+	 System.out.println( sum);
+	}
+}
